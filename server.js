9086ef6c06ee45e7078fb521110555172c9eb3d3
Prabhjyot Gambhir <knowprabhjyot@gmail.com>
2022-11-29 10:33:14 -0800

    added port

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
-const PORT = 4000;
+
+// This tells the server to use there own port if they wnat
+
+const PORT = process.env.PORT ||  4000;
 const mongoose = require("mongoose");
 const userRoutes=  require("./routes/user");
 
